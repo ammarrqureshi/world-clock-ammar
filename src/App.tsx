5ed7421454aa515595a586ec7165c630408b7566
ammarrqureshi <mamqureshi786@gmail.com>
2024-01-09 17:33:44 +0500

    feat: dayjs first function added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-
+import dayjs from "dayjs"
 export const App = () => {
 
   return (
     <>
       <div className="bg-slate-500">
-
-
-        Day clock code here
-      </div>   </>
+        {dayjs().startOf('month').add(1, 'day').set('year', 2018).format('YYYY-MM-DD HH:mm:ss')}      </div>   </>
   )
 }
 
