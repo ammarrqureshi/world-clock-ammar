1e8cbc56dd1d6fc658f29e5d5cd2b9cd0a7da7ad
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-07 01:53:51 +0500

    box style fixed

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export const App = () => {
   return (
     <>
       <div className="flex justify-center h-screen w-full items-center">
-        <div className="flex flex-col bg-red-500 justify-center items-center border border-gray-900">
+        <div className="flex flex-col  justify-center items-center border border-gray-900 p-8 rounded-xl">
           <Button />
           <LiveClock></LiveClock>
           <Date />
