649a239278b8d7320a5b81d12e30931e40c46702
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-06 22:57:39 +0500

    button comp init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from "./components/Button";
 import { LiveClock } from "./components/LiveClock";
 export const App = () => {
   return (
     <>
       <div className="">
+        <Button/>
         <LiveClock></LiveClock>
+        
       </div>
     </>
   );
