97be1a1fe501a398da94dc551cf4f12aa7470f3f
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-08 19:35:51 +0500

    all functions finalized, styling left

--- src/components/TimeZone.tsx
+++ src/components/TimeZone.tsx
@@ -1,32 +1,37 @@
 import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
-import React, { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
-export const TimeZone = (time: string) => {
-  const [selectedRegion, setSelectedRegion] = useState("America/Toronto");
+export const TimeZone = () => {
+  const [selectedRegion, setSelectedRegion] = useState(dayjs.tz.guess());
   dayjs.extend(utc);
   dayjs.extend(timezone);
   dayjs.tz.setDefault(selectedRegion);
 
   const supportedTimezones = Intl.supportedValuesOf("timeZone");
 
-
-
-
+  const getSelectedTimezone = (event: ChangeEvent<HTMLSelectElement>) => {
+    event.preventDefault();
+    setSelectedRegion(event.target.value);
+  };
 
   return (
     <div>
       {selectedRegion}
       <div>
         <form action="">
-        <select id="timezones" name="timezones">
-        {supportedTimezones.map((timezone:string, index:any) => (
-          <option key={index} value={timezone}>
-            {timezone}
-          </option>
-        ))}
-      </select>
+          <select
+            onChange={getSelectedTimezone}
+            id="timezones"
+            name="timezones"
+          >
+            {supportedTimezones.map((timezone: string, index: number) => (
+              <option key={index} value={timezone}>
+                {timezone}
+              </option>
+            ))}
+          </select>
         </form>
       </div>
     </div>
