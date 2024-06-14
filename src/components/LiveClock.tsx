4a78de961e4688dc0f6bfa7a1960b850afa958d7
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-15 00:15:15 +0500

    fix : today final

--- src/components/LiveClock.tsx
+++ src/components/LiveClock.tsx
@@ -7,6 +7,7 @@ export const LiveClock = () => {
   const [time, setTime] = useState(dayjs.tz().format("HH:mm:ss"));
   dayjs.extend(utc);
   dayjs.extend(timezone);
+
   useEffect(() => {
     const timeInterval = setInterval(() => {
       setTime(dayjs.tz().format("HH:mm:ss"));
