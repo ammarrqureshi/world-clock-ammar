578e9cb96020b299463cc38313771d2d190868e0
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-22 00:28:33 +0500

    date fixed

--- src/components/Date.tsx
+++ src/components/Date.tsx
@@ -1,8 +1,16 @@
 import * as dayjs from "dayjs";
+import { useContext, useEffect, useState } from "react";
+import {
+  TimezoneContext,
+  TimezoneContextProps,
+} from "../contexts/TimezoneContext";
 
 export const Date = () => {
+  const timezone = useContext(TimezoneContext) as TimezoneContextProps;
+  const [date, setDate] = useState(dayjs.tz().format("dddd, DD MMMM, YYYY"));
 
-
-  const date = dayjs.tz().format("dddd, DD MMMM, YYYY");
+  useEffect(() => {
+    setDate(dayjs.tz().format("dddd, DD MMMM, YYYY"));
+  }, [timezone]);
   return <div>{date}</div>;
 };
