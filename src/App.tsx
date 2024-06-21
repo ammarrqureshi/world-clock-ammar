d3dc83d6541e5687d8f09ec2532c027faeca9390
ammarrqureshi <mamqureshi786@gmail.com>
2024-06-21 23:26:03 +0500

    fixes in icon button

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export const App = () => {
     <>
       <div className="flex justify-center h-screen w-full items-center">
         <div className="flex flex-col  justify-center items-center br-sketch border-4 border-gray-900 p-8 rounded-xl">
-          <TimeZone />
+          {/* <TimeZone /> */}
           <EditButton />
 
           <LiveClock></LiveClock>
--- src/components/ui/EditButton.tsx
+++ src/components/ui/EditButton.tsx
@@ -2,7 +2,7 @@ import editIcon from "../../assets/edit-icon-1.png";
 
 export const EditButton = () => {
   return (
-    <button className="max-w-6 p-2 box-content hover:bg-black/15">
+    <button className="max-w-6 p-2 box-content hover:bg-black/15 rounded-2xl br-sketch border-2 border-black ">
       <div>
         <img src={editIcon} alt="Edit" />
       </div>
